ee4212933be8c92f6e7e77bbd4edae7f98c15ac3
Jakub Schulz <jakub104@Dell-Komputer.localdomain>
2019-04-08 22:29:59 +0200

    Nowy projekt w 3min

---
+++ todolist/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class ToDoList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ToDoList title='My Stuff' />
+      </div>
+    );
+  }
+}
+
+export default App;
